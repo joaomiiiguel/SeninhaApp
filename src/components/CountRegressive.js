1b89168a62a09830a51df130a94f16a42ab1c181
João Miguel <joaomiguel.farias@gmail.com>
2021-12-14 14:36:43 -0300

    Components Home

--- src/components/CountRegressive.js
+++ src/components/CountRegressive.js
@@ -1,19 +1,51 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-
 export default function CountRegressive() {
     return (
         <View style={styles.containerCount}>
-            <Text>Próximo Sorteio:</Text>
+            <View style={{display: 'flex', flexDirection: 'row', marginTop: 5}}><Text style={{color: '#FFF'}}>Próximo Sorteio:</Text><Text style={{color: '#FFF', fontWeight: 'bold'}}> 26 de Setembro 2021</Text></View>
+            <View style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent:'center'}}>
+                <View style={styles.cardNumberCount}>
+                    <Text style={styles.numberCount}>00</Text>
+                    <Text style={{color: '#FFF', fontSize: 12}}>dias</Text>
+                </View>
+                <Text style={{color: '#FFF', fontWeight:'bold', fontSize: 25, marginBottom: 15}}>:</Text>
+                <View style={styles.cardNumberCount}>
+                    <Text style={styles.numberCount}>00</Text>
+                    <Text style={{color: '#FFF', fontSize: 12}}>horas</Text>
+                </View>
+                <Text style={{color: '#FFF', fontWeight:'bold', fontSize: 25, marginBottom: 15}}>:</Text>
+                <View style={styles.cardNumberCount}>
+                    <Text style={styles.numberCount}>00</Text>
+                    <Text style={{color: '#FFF', fontSize: 12}}>minutos</Text>
+                </View>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     containerCount:{
-        width: '100%',
         backgroundColor: '#1D72DE',
         borderRadius: 5,
-        padding: 10,
+        padding: 15,
+        alignItems: 'center',
+        marginTop: 20
+    },
+    cardNumberCount:{
+        alignItems: 'center',
+        width: '20%',
+        marginRight: 15,
+        marginLeft: 15
+    },
+    numberCount:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        backgroundColor: '#003065',
+        marginTop: 15,
+        padding: 8,
+        borderRadius: 5,
+        color: '#FFF',
+        
     }
 })
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, StatusBar, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, StatusBar, View, TouchableOpacity, Image } from 'react-native'
 import { useFonts } from 'expo-font';
+import { theme } from '../globalstyles';
+
 
 import CountRegressive from '../components/CountRegressive';
+import ListaJogada from '../components/ListaJogada';
+
+import iconBtnNova from '../../assets/image-7.png'
+import iconBtnPremi from '../../assets/image-8.png'
+import iconBtnResult from '../../assets/image-9.png'
 
 export default function HomeScreen() {
     const user = 'Miguel'
@@ -23,6 +30,25 @@ export default function HomeScreen() {
                 <Text style={{ fontFamily: 'RajdhaniRegular', color: '#FFF' }}>Já fez sua aposta hoje?</Text>
             </View>
             <CountRegressive />
+            <View style={{display: 'flex', alignItems: 'center'}} >
+                <Text style={styles.titleContent}>Sua Jogada</Text>
+                <ListaJogada />
+            </View>
+            <View style={styles.contentBtns}>
+                <TouchableOpacity style={styles.BtnGrande}>
+                    <Image source={iconBtnNova} style={styles.imgBtn}/>
+                    <Text style={styles.titleBtn}>Nova Aposta</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.BtnGrande}>
+                    <Image source={iconBtnPremi} style={styles.imgBtn}/>
+                    <Text style={styles.titleBtn}>Premiação</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.BtnGrande}>
+                    <Image source={iconBtnResult} style={styles.imgBtn}/>
+                    <Text style={styles.titleBtn}>Resultado</Text>
+                </TouchableOpacity>
+                
+            </View>
         </SafeAreaView>
     )
 }
@@ -31,13 +57,45 @@ const styles = StyleSheet.create({
     containerHome: {
         flex: 1,
         display: 'flex',
-        backgroundColor: '#003065',
-        paddingHorizontal: 25,
-        paddingTop: StatusBar.currentHeight + 10,
+        backgroundColor: theme.colors.primary75,
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingTop: 20,
     },
     titleName: {
         fontFamily: 'RajdhaniBold',
         fontSize: 28,
-        color: '#FFF'
+        color: theme.colors.white
+    },
+    titleContent: {
+        fontFamily: 'RajdhaniBold',
+        marginTop: 15,
+        fontSize: 20,
+        color: theme.colors.white,
+    },
+    BtnGrande:{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: theme.colors.white,
+        borderRadius: 5,
+        padding: 8,
+        width: '32%'
+    },
+    titleBtn:{
+        fontFamily: 'RajdhaniBold',
+        fontSize: 17,
+        color: theme.colors.primary75
+    },  
+    imgBtn:{
+        width: 50,
+        height: 50,
+        margin: 5
+    },
+    contentBtns:{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 30
     }
 })
